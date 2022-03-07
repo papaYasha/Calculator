75fcc87eaa1b291f88fece7c8862ba45627c3739
papaYasha <90447243+papaYasha@users.noreply.github.com>
2022-03-07 11:39:44 +0300

    finish work with UI, create IBOutlets/actions

--- Calculator/ViewController.swift
+++ Calculator/ViewController.swift
@@ -9,14 +9,19 @@ import UIKit
 
 class ViewController: UIViewController {
 
-    @IBOutlet weak var mainStackView: UIStackView!
+    @IBOutlet weak var displayLabel: UILabel!
     
     override func viewDidLoad() {
         super.viewDidLoad()
-//        let figureView = UIView()
-//        figureView.backgroundColor = .black
-//        figureView.frame.size = CGSize(width: view.frame.width, height: view.frame.height / 3)
-//        mainStackView.addArrangedSubview(figureView)
+        
     }
+    
+    @IBAction func calcButtonPressed(_ sender: UIButton) {
+
+    }
+    
+    @IBAction func numButtonPressed(_ sender: UIButton) {
+    }
+    
 }
 
