5bad4d3dcf5aba83886d4cc2a14b5ddf670d0cab
papaYasha <90447243+papaYasha@users.noreply.github.com>
2022-03-07 11:55:37 +0300

    implement input number from keypad

--- Calculator/ViewController.swift
+++ Calculator/ViewController.swift
@@ -11,16 +11,27 @@ class ViewController: UIViewController {
 
     @IBOutlet weak var displayLabel: UILabel!
     
+    var isFinishedTypingNumber: Bool = true
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         
     }
     
     @IBAction func calcButtonPressed(_ sender: UIButton) {
+        isFinishedTypingNumber = true
 
     }
     
     @IBAction func numButtonPressed(_ sender: UIButton) {
+        if let numValue = sender.currentTitle {
+            if isFinishedTypingNumber {
+                displayLabel.text = numValue
+                isFinishedTypingNumber = false
+            } else {
+                displayLabel.text = displayLabel.text! + numValue
+            }
+        }
     }
     
 }
